da85a5b27d48f3d16e6e20cc6a3f1c8d42e03f58
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2023-02-27 17:00:06 -0300

    Delete EPOO(416-01)SEG-MANHA/2023-02-20 directory

--- EPOO(416-01)SEG-MANHA/2023-02-20/controle/Programa05.java
+++
@@ -1,9 +0,0 @@
-package controle;
-
-public class Programa05 {
-	public static void main(String[] args) {
-		System.out.println("Java é o poder!!!");
-		System.out.println("OO é a palavra de ordem!");
-		System.out.println("O primeiro programa é inesquecível!");
-	}
-}
